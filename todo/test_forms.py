8b1b1f7704b4ae5d65570ef51df091ef463dfe5e
Rhys Johnston <rhysjj111@gmail.com>
2024-01-28 20:09:48 +0000

    Add tests to test_views, test_models and test_forms

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -17,3 +17,4 @@ class TestItemForm(TestCase):
     def test_fields_are_explicit_in_form_metaclass(self):
         form = ItemForm()
         self.assertEqual(form.Meta.fields, ['name', 'done'])
+        
